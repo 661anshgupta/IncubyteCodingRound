91949fa67532bb5e4c9aa1522962e4db7b686c40
661anshgupta <160109971+661anshgupta@users.noreply.github.com>
2024-02-17 05:57:50 +0000

    Support different delimiters

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ function add(numbers) {
   if (!numbers) {
     return 0;
   }
-  const splitNumbers = numbers.split(/[\n,]/);
-  //We have to filter out empty strings
+
+  let delimiter = ",";
+
+  //Checking if the number starts with "//""
+  if (numbers.startsWith("//")) {
+    const delimiterEndIndex = numbers.indexOf("\n");
+    // storing new delimeter
+    delimiter = numbers.substring(2, delimiterEndIndex);
+    numbers = numbers.substring(delimiterEndIndex + 1);
+  }
+  // Making new regular expresion with the new delimeter if any
+  const newRegex = new RegExp(`[${delimiter}\\n]`);
+  const splitNumbers = numbers.split(newRegex);
   const validInput = splitNumbers.filter((num) => num !== "" && !isNaN(num));
   const negatives = validInput.filter((num) => parseInt(num) < 0);
   if (negatives.length > 0) {
@@ -18,7 +29,7 @@ try {
   console.log(add("1,5"));
   console.log(add("1,\n"));
   console.log(add("1,\n2,3"));
-  console.log(add("-1,2"));
+  console.log(add("//;\n1;2"));
 } catch (error) {
   console.error(error);
 }
