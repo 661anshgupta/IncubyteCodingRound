f105fa1829cb99654d72e6bf9aa02e13f49fbcea
661anshgupta <160109971+661anshgupta@users.noreply.github.com>
2024-02-17 05:39:37 +0000

    Handled negative numbers

--- main.js
+++ main.js
@@ -5,8 +5,20 @@ function add(numbers) {
   const splitNumbers = numbers.split(/[\n,]/);
   //We have to filter out empty strings
   const validInput = splitNumbers.filter((num) => num !== "" && !isNaN(num));
+  const negatives = validInput.filter((num) => parseInt(num) < 0);
+  if (negatives.length > 0) {
+    throw "negative numbers are not allowed " + negatives.join(",");
+  }
   return validInput.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
-console.log(add("1,\n"));
-console.log(add("1,\n2,3"));
+try {
+  console.log(add(""));
+  console.log(add("1"));
+  console.log(add("1,5"));
+  console.log(add("1,\n"));
+  console.log(add("1,\n2,3"));
+  console.log(add("-1,2"));
+} catch (error) {
+  console.error(error);
+}
