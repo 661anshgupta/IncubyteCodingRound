2aee457ccc7d45fbdc2eb9d72d62e3803e470487
661anshgupta <160109971+661anshgupta@users.noreply.github.com>
2024-02-17 05:35:46 +0000

    Allow the add method to handle new lines between numbers (instead of commas).

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ function add(numbers) {
   if (!numbers) {
     return 0;
   }
-  return numbers.split(",").reduce((sum, num) => sum + parseInt(num), 0);
+  const splitNumbers = numbers.split(/[\n,]/);
+  //We have to filter out empty strings
+  const validInput = splitNumbers.filter((num) => num !== "" && !isNaN(num));
+  return validInput.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
-console.log(add(""));
-console.log(add("2"));
-console.log(add("1,5"));
+console.log(add("1,\n"));
+console.log(add("1,\n2,3"));
